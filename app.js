28ec237e8b4a35e1cfa63ca8f865b71c018f77c2
Anees ur Rehman <125833255+anees-rehman1@users.noreply.github.com>
2026-04-15 19:33:37 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>Hello from Modern Jenkins Pipeline (2024)!</h1>');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
+
+module.exports = server;
